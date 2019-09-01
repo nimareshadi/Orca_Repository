8d3538dd3b8edb15883bf1e1387334110bb2d1b3
admin <nima.reshadi.ce@gmail.com>
2019-09-01 20:25:36 +0430

    init

---
+++ Language/src/main/java/net/nimiro/language/LanguageRepository.java
@@ -0,0 +1,7 @@
+package net.nimiro.language;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LanguageRepository extends MongoRepository<LanguageEntity, String> {
+
+}
--- src/main/java/com/nimi/orcamicroserver/EnLanguage/EnRepository.java
+++
@@ -1,4 +0,0 @@
-package com.nimi.orcamicroserver.EnLanguage;
-
-public class EnRepository {
-}
