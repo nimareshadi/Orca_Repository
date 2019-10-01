8dd82b6a4f83bffc8cd93c4c820d6958aaec3681
admin <nima.reshadi.ce@gmail.com>
2019-10-01 20:03:46 +0330

    zuul is added to 'word' webservice

--- ocra/gateway/src/main/java/net/nimiro/gateway/GatewayApplication.java
+++ ocra/gateway/src/main/java/net/nimiro/gateway/GatewayApplication.java
@@ -1,16 +1,19 @@
-package net.nimiro;
+package net.nimiro.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
-@EnableFeignClients
-public class LanguageConsumerApplication {
+@RestController
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LanguageConsumerApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
--- ocra/gateway/src/test/java/net/nimiro/gateway/GatewayApplicationTests.java
+++ ocra/gateway/src/test/java/net/nimiro/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package net.nimiro;
+package net.nimiro.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServerApplicationTests {
+public class GatewayApplicationTests {
 
     @Test
     public void contextLoads() {
